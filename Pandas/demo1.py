c7db848aceb3bb097111f1da3b6d8d3c32172c28
Jamilé Obeid <jamileemhd@gmail.com>
2024-11-23 19:32:42 +0400

    Update demo1.py

--- Pandas/demo1.py
+++ Pandas/demo1.py
@@ -4,7 +4,20 @@ df = pd.read_csv('TeddyBallgame.csv')
 
 print(df.loc[0]) # print row 0
 print(df.loc[0:4]) # print rows 0-4
+
 print(df['Year']) # print the column labeled 'Year'
+
 print(df[['Age','HomeRuns']]) # print two columns
 print(df['Age'][0]) # print the first row in ‘Age'
 print(df[['Age','HomeRuns']][0:9]) #rows 0-9 in the given columns
+
+print(df.info()) # printing the data information
+
+print(df['GamesPlayed'].sum())
+print(df['GamesPlayed'].max()) # maximum value
+print(df['GamesPlayed'].min()) # minimum value
+
+print(df['HomeRuns'].mean()) # average of values 
+print(df['HomeRuns'].median())
+
+print(df['BattingAverage'].std())
